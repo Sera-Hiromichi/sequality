63ae368589087f000a5c48bf320fb622abe4b156
Hiromichi Sera <h-sera21@ixdlab.info>
2025-06-30 10:16:16 +0900

    repair AppTest

--- app/src/test/java/org/example/AppTest.java
+++ app/src/test/java/org/example/AppTest.java
@@ -18,7 +18,7 @@ public class AppTest {
   @Test
   public void testAppReturnHelloWorld() {
     App classUnderTest = new App();
-    assertEquals("Hello World!", classUnderTest.getGreeding());
+    assertEquals("Hello World!", classUnderTest.getGreeting());
   }
 }
 
